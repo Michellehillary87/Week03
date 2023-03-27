a61b721a8c63d529a674fe6f4d26e4fceb70df20
michelle <michelle.hillary@student.umn.ac.id>
2023-03-27 14:22:05 +0900

    Adding class constructor

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,11 +1,40 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+import java.lang.Math.PI
+class Aquarium (var length: Int = 100,
+                var width: Int = 20,
+                var height: Int = 40) {
     fun printSize() {
-        println("Width: $width cm " + "Length: $length cm " + "Height: $height cm ")
+        println(
+            "Width: $width cm " +
+                    "Length: $length cm " +
+                    "Height: $height cm "
+        )
+        // 1 liter = 1000 cm ^ 3
+        println("Volume: $volume liters")
     }
+
+    init {
+        println("aquarium initializing")
+    }
+//    init {
+//        // 1 liter = 1000 cm^3
+//        println("Volume: ${width * length * height / 1000} liters")
+//    }
+    constructor(numberOfFish: Int) : this()
+    {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+    var volume: Int
+        get() = width * height * length / 1000 // 1000 cm ^ 3 = 1 liter
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 }
-// Hasilnya : Width: 20 cm Length: 100 cm Height: 40 cm
+    // Dimension in cm
+    //var width: Int = 20
+    //var height: Int = 40
+    //var length: Int = 100
+    //Hasilnya : Width: 20 cm Length: 100 cm Height: 40 cm
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,14 +1,59 @@
 package example.myapp
 
 import java.lang.Math.PI
+
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume  = 70
+    aquarium6.printSize()
+    //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+//    val aquarium1 = Aquarium()
+//    aquarium1.printSize()
+//    // default height and length
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//    // default width
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//    // everything custom
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
 }
-fun main(){
+fun main() {
     buildAquarium()
 }
+// Step 1: Create a constructor
 //Width: 20 cm Length: 100 cm Height: 40 cm
 //Width: 20 cm Length: 100 cm Height: 60 cm
+
+// Step 2: Add init blocks
+//aquarium initializing
+//Volume: 80 liters
+//Width: 20 cm Length: 100 cm Height: 40 cm
+//aquarium initializing
+//Volume: 100 liters
+//Width: 25 cm Length: 100 cm Height: 40 cm
+//aquarium initializing
+//Volume: 77 liters
+//Width: 20 cm Length: 110 cm Height: 35 cm
+//aquarium initializing
+//Volume: 96 liters
+//Width: 25 cm Length: 110 cm Height: 35 cm
+
+//Step 3: Learn about secondary constructors
+//aquarium initializing
+//Width: 20 cm Length: 100 cm Height: 31 cm
+//Volume: 62 liters
+
+//Step 4: Add a new property getter
+//aquarium initializing
+//Width: 20 cm Length: 100 cm Height: 31 cm
+//Volume: 62 liters
+
+//Step 5: Add a property setter
+//aquarium initializing
+//Width: 20 cm Length: 100 cm Height: 31 cm
+//Volume: 62 liters
+//Width: 20 cm Length: 100 cm Height: 35 cm
+//Volume: 70 liters
